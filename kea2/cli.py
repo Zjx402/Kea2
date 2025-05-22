d08e910b6cc1bf7dc98a9446ff269859eb633cfb
XixianLiang <51265902107@stu.ecnu.edu.cn>
2025-05-22 16:20:45 +0800

    fix: cleaning code in cli

--- kea2/cli.py
+++ kea2/cli.py
@@ -47,83 +47,13 @@ def cmd_run(args):
     run(args)
 
 
-def cmd_install(args):
-    pass
-
-
-def cmd_uninstall(args):
-    pass
-
-
-def cmd_start(args):
-    pass
-
-
-def cmd_stop(args):
-    pass
-
-
-def cmd_current(args):
-    pass
-
-
-def cmd_doctor(args):
-    pass
-
-
-# def cmd_version(args):
-#     print(__version__)
-
-
 _commands = [
     # dict(action=cmd_version, command="version", help="show version"),
     dict(
         action=cmd_init,
         command="init",
         help="init the Kea2 project in current directory",
-    ),
-    # dict(
-    #     action=cmd_run,
-    #     command="run",
-    #     help="run kea2",
-    #     flags=[
-    #         dict(args=["args"], nargs=argparse.REMAINDER),
-    #     ],
-    # ),
-    # dict(
-    #     action=cmd_install,
-    #     command="",
-    #     help="install packages",
-    #     flags=[
-    #         dict(args=["url"], help="package url"),
-    #     ],
-    # ),
-    # dict(
-    #     action=cmd_uninstall,
-    #     command="uninstall",
-    #     help="uninstall packages",
-    #     flags=[
-    #         dict(args=["--all"], action="store_true", help="uninstall all packages"),
-    #         dict(args=["package_name"], nargs="*", help="package name"),
-    #     ],
-    # ),
-    # dict(
-    #     action=cmd_start,
-    #     command="start",
-    #     help="start application",
-    #     flags=[dict(args=["package_name"], type=str, nargs=None, help="package name")],
-    # ),
-    # dict(
-    #     action=cmd_stop,
-    #     command="stop",
-    #     help="stop application",
-    #     flags=[
-    #         dict(args=["--all"], action="store_true", help="stop all"),
-    #         dict(args=["package_name"], nargs="*", help="package name"),
-    #     ],
-    # ),
-    # dict(action=cmd_current, command="current", help="show current application"),
-    # dict(action=cmd_doctor, command="doctor", help="detect connect problem"),
+    )
 ]
 
 
@@ -132,9 +62,7 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("-d", "--debug", action="store_true",
-                        help="show log")
-    parser.add_argument('-s', '--serial', type=str,
-                        help='device serial number')
+                        help="show detail log")
 
     subparser = parser.add_subparsers(dest='subparser')
 
