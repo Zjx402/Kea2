d2056b59a360d487ba9719306510eeba61a98a29
majuzi <2318332846@qq.com>
2025-05-28 11:07:47 +0800

    fix: Tree pruning block

--- kea2/assets/fastbot_configs/widget.block.py
+++ kea2/assets/fastbot_configs/widget.block.py
@@ -16,3 +16,18 @@ def global_block_widgets(d: "Device"):
 def block_sth(d: "Device"):
     # Important: function shold starts with "block_"
     return [d(text="widgets to block"), d.xpath(".//node[@text='widget to block']")]
+
+
+def global_block_tree(d: "Device"):
+    """
+    global block trees.
+    return the root of trees you want to block globally
+    only available in mode `u2 agent`
+    """
+    return [d(resourceId="it.feio.android.omninotes.alpha:id/toolbar")]
+
+
+@precondition(lambda d: d(text="In the home page").exists)
+def block_tree_sth(d: "Device"):
+    # Important: function shold starts with "block_tree_"
+    return [d(text="trees to block"), d.xpath(".//node[@text='tree to block']")]
