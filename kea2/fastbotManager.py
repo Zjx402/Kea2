8c3622249f19391a6517897abcfd10573cc30c05
XixianLiang <51265902107@stu.ecnu.edu.cn>
2025-09-19 10:32:55 +0800

    fix: push abl.strings in project configs

--- kea2/fastbotManager.py
+++ kea2/fastbotManager.py
@@ -7,7 +7,7 @@ from packaging.version import parse as parse_version
 from uiautomator2.core import HTTPResponse, _http_request
 from kea2.adbUtils import ADBDevice, ADBStreamShell_V2
 from pathlib import Path
-from kea2.utils import getLogger
+from kea2.utils import getLogger, getProjectRoot
 
 
 from typing import IO, TYPE_CHECKING, Dict
@@ -70,14 +70,15 @@ class FastbotManager:
             "/data/local/tmp/x86_64/libfastbot_native.so",
         )
 
+        cwd = getProjectRoot()
         whitelist = self.options.act_whitelist_file
         blacklist = self.options.act_blacklist_file
         if bool(whitelist) ^ bool(blacklist):
             if whitelist:
-                file_to_push = cur_dir.parent / 'configs' / 'awl.strings'
+                file_to_push = cwd / 'configs' / 'awl.strings'
                 remote_path = whitelist
             else:
-                file_to_push = cur_dir.parent / 'configs' / 'abl.strings'
+                file_to_push = cwd / 'configs' / 'abl.strings'
                 remote_path = blacklist
 
             self.dev.sync.push(
